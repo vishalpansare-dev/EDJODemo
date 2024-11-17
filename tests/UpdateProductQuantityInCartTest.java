870df0f3a9e9a63fa6ddd259c04f6f6b8ad8948b
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-17 23:27:31 +0530

    Create tests/UpdateProductQuantityInCartTest.java

---
+++ tests/UpdateProductQuantityInCartTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UpdateProductQuantityInCartTest {
+    @Test
+    public void testUpdateProductQuantity() {
+        // Implement test logic here
+        assertTrue(true);
+    }
+}
