0d73fb7095a39c5ddfd130761bf3a37655bc91bc
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-17 19:17:42 +0530

    Update src/test/java/com/edwardjones/cksn_ui_tests/_tsc/PaymentTransferSteps.java

--- src/test/java/com/edwardjones/cksn_ui_tests/_tsc/PaymentTransferSteps.java
+++ src/test/java/com/edwardjones/cksn_ui_tests/_tsc/PaymentTransferSteps.java
@@ -21,10 +21,9 @@ public class PaymentsTransferSteps {
     }
 
     @Step("Verify checkout details")
+    @Step("Verify checkout details")
     public void verifyCheckoutDetails() {
-        public void verifyCheckoutDetails() {
         // Implementation for verifying checkout details
         paymentPage.verifyCheckoutDetails();
-    }
     }
 }
