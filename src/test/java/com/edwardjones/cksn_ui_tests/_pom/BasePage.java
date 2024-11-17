4b18807ec4ebee5418f9efda2743c15a894d66c8
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-17 15:13:45 +0530

    Update src/test/java/com/edwardjones/cksn_ui_tests/_pom/BasePage.java

--- src/test/java/com/edwardjones/cksn_ui_tests/_pom/BasePage.java
+++ src/test/java/com/edwardjones/cksn_ui_tests/_pom/BasePage.java
@@ -3,6 +3,9 @@ package com.edwardjones.cksn_ui_tests._pom;
 public class BasePage {
 public void clickLogout() {
     // Logic to click the logout button
+        // Example: driver.findElement(logoutButton).click();
+        // (Assuming you have a WebDriver instance)
+        // You might also want to add a wait here for the action to complete.
 }
 
 }
