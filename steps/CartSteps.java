3bd466952af4ef8be95ef10f136961021272be1c
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-17 23:27:34 +0530

    Create steps/CartSteps.java

---
+++ steps/CartSteps.java
@@ -0,0 +1,8 @@
+import io.cucumber.java.en.When;
+
+public class CartSteps {
+    @When("I update the product quantity in the cart")
+    public void updateProductQuantity() {
+        // Implement step definition here
+    }
+}
