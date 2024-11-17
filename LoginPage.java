39b4cc8def334753c00894645cf2293943263f19
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-17 14:55:46 +0530

    Create LoginPage.java

---
+++ LoginPage.java
@@ -0,0 +1,11 @@
+public class LoginPage {
+    // Method to perform login
+    public void login(String username, String password) {
+        // Logic for login
+    }
+
+    // Method to get logout message
+    public String getLogoutMessage() {
+        return "Logged out successfully";
+    }
+}
