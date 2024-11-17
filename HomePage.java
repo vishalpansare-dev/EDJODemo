3367efda2e9009b07a3a4fe90c7ba231635197fa
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-17 14:55:48 +0530

    Create HomePage.java

---
+++ HomePage.java
@@ -0,0 +1,12 @@
+public class HomePage {
+    // Method to click logout
+    public void clickLogout() {
+        // Logic to click logout button
+    }
+
+    // Method to check redirection to login page
+    public boolean isRedirectedToLoginPage() {
+        // Logic to verify redirection
+        return true; // Placeholder
+    }
+}
