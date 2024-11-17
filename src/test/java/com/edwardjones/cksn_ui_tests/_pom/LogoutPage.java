6e1dcb38b11c02b3aebfa033c3583fc8317cb40f
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-17 15:14:50 +0530

    Create src/test/java/com/edwardjones/cksn_ui_tests/_pom/LogoutPage.java

---
+++ src/test/java/com/edwardjones/cksn_ui_tests/_pom/LogoutPage.java
@@ -0,0 +1,14 @@
+package com.edwardjones.cksn_ui_tests._pom;
+
+public class LogoutPage {
+    // Method to click the logout button
+    public void clickLogout() {
+        // Code to click the logout button
+    }
+
+    // Method to verify logout success message
+    public String getLogoutMessage() {
+        // Code to get the logout success message
+        return "Logged out successfully";
+    }
+}
