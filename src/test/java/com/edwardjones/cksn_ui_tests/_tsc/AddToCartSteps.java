697ca24c603cd0a90815a793f8b0f91cf352e6eb
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-17 22:49:17 +0530

    Create src/test/java/com/edwardjones/cksn_ui_tests/_tsc/AddToCartSteps.java

---
+++ src/test/java/com/edwardjones/cksn_ui_tests/_tsc/AddToCartSteps.java
@@ -0,0 +1,20 @@
+package com.edwardjones.cksn_ui_tests._tsc;
+
+import net.serenitybdd.core.steps.ScenarioSteps;
+import net.thucydides.core.annotations.Step;
+import com.edwardjones.cksn_ui_tests._pom.AddToCartPage;
+
+public class AddToCartSteps extends ScenarioSteps {
+
+    AddToCartPage addToCartPage;
+
+    @Step("Add item to cart")
+    public void addItemToCart() {
+        addToCartPage.clickAddToCart();
+    }
+
+    @Step("Verify cart icon updates")
+    public void verifyCartIconUpdates() {
+        assert addToCartPage.isCartIconUpdated();
+    }
+}
