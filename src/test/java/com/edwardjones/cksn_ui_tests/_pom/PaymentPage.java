4934c6ec9fb71022e50d0fc49f311b8be993641b
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-17 19:13:47 +0530

    Update src/test/java/com/edwardjones/cksn_ui_tests/_pom/PaymentPage.java

--- src/test/java/com/edwardjones/cksn_ui_tests/_pom/PaymentPage.java
+++ src/test/java/com/edwardjones/cksn_ui_tests/_pom/PaymentPage.java
@@ -5,8 +5,26 @@ package com.edwardjones.cksn_ui_tests._pom;
 public class PaymentPage extends BasePage {
         @FindBy(xpath = "//h2[text()='Accounts']")
         private WebElementFacade accountsHeading;
-        @FindBy(xpath = "//div|@class='ej-amc-transfers-dropdown-no-data']")
-        private WebElementFacade textInvalidAccountSearchErmorMsg;
+        @FindBy(xpath = "//div[@class='ej-amc-transfers-dropdown-no-data']")
+        private WebElementFacade textInvalidAccountSearchErrorMsg;
+
+    @FindBy(xpath = "//div[@class='ej-amc-error-retry__link']")
+        private WebElementFacade buttonRetryAccountSearch;
+
+    @FindBy(xpath = "//p[@class='ej-amc-error-retry__text']")
+        private WebElementFacade textAccountSearchRetrieveErrorMsg;
+
+    public void completeCheckout() {
+        // Code to complete checkout
+    }
+
+    public void verifyNoItemsInCart() {
+        // Implement logic to verify no items in cart
+    }
+
+    public void applyPromoCode(String promoCode) {
+        // Logic to apply promo code during checkout
+    }
         @FindBy(xpath = "//div[@class='ej-amc-error-retry__link']ª)
         private WebElementFacade buttonRetryAccountSearch;
         @FindBy(xpath = "//p[@class='ej-amc-error-retry__text']ª)
