a1db5016b263409397e029ab1511c2f1b346787d
Vishal Pansare <70095753+vishalpansare-dev@users.noreply.github.com>
2024-11-17 19:15:33 +0530

    Create src/test/java/com/edwardjones/cksn_ui_tests/_tsc/PaymentTransferSteps.java

---
+++ src/test/java/com/edwardjones/cksn_ui_tests/_tsc/PaymentTransferSteps.java
@@ -0,0 +1,23 @@
+package com.edwardjones.cksn_ui_tests._tsc;
+
+import net.serenitybdd.core.annotations.Step;
+import com.edwardjones.cksn_ui_tests._pom.PaymentPage;
+
+public class PaymentsTransferSteps {
+    private PaymentPage paymentPage;
+
+    @Step("Add item to cart")
+    public void addItemToCart() {
+        // Implementation for adding an item to cart
+    }
+
+    @Step("Proceed to checkout")
+    public void proceedToCheckout() {
+        // Implementation for proceeding to checkout
+    }
+
+    @Step("Verify checkout details")
+    public void verifyCheckoutDetails() {
+        // Implementation for verifying checkout details
+    }
+}
